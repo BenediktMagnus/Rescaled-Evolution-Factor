798d40aaa09d1407c1158df971486d1dd9b976c3
Benedikt Magnus <magnus@magnuscraft.de>
2018-06-20 20:23:22 +0200

    Removed EvoGUI integration.

--- control.lua
+++ control.lua
@@ -15,13 +15,11 @@ script.on_configuration_changed(function(data)
  end)
  
  script.on_load(function()
-	if remote.interfaces.EvoGUI ~= nil then registriere_sensoren() end
+	
  end)
 
 function pollutiontest(event)
-    if game.tick % 60 == 24 then
-		if remote.interfaces.EvoGUI ~= nil then registriere_sensoren() end
-
+	if game.tick % 60 == 24 then
 		local summe = 0
 		local anzahl = 0
       
@@ -61,12 +59,7 @@ function pollutiontest(event)
 		end
 	  
 		game.evolution_factor = faktor	  
-		
-		if remote.interfaces.EvoGUI ~= nil then
-			remote.call("EvoGUI", "update_remote_sensor", "rescaled_evolution_factor_sensor_pollution", {"", {"rescaled_evolution_factor_sensor_pollution_prefix"}, " ", summe_formatieren(summe)})
-			remote.call("EvoGUI", "update_remote_sensor", "rescaled_evolution_factor_sensor_spawner", {"", {"rescaled_evolution_factor_sensor_spawner_prefix"}, " ", global.pollution_spawner})
-			remote.call("EvoGUI", "update_remote_sensor", "rescaled_evolution_factor_sensor_anteile", {"", {"rescaled_evolution_factor_sensor_anteile_prefix"}, " ", string.format("%.1f", vergangene_zeit * 10).."%|", string.format("%.1f", pollution_spawner_anzahl * 10).."%|", string.format("%.1f", pollution_summe * 10).."%"})
-		end
+
 	end
    
     if (game.tick % 216000 == 25 and global.pollution_spawner > 0) then
@@ -78,47 +71,6 @@ function spawnertot(event)
    if (global.pollution_spawner < 60 and (event.entity.name == "spitter-spawner" or event.entity.name == "biter-spawner")) then
       global.pollution_spawner = global.pollution_spawner + 1
    end   
-end
-
-function registriere_sensoren()
-	if global.ist_registriert == nil then
-		local sensor_data = {mod_name = "Evolve Factor",
-							 name = "rescaled_evolution_factor_sensor_pollution",
-							 text = {"rescaled_evolution_factor_sensor_pollution_display", 42},
-							 caption = {"rescaled_evolution_factor_sensor_pollution_caption"}}
-   
-		remote.call("EvoGUI", "create_remote_sensor", sensor_data)
-   
-		sensor_data = {mod_name = "Evolve Factor",
-					   name = "rescaled_evolution_factor_sensor_spawner",
-					   text = {"rescaled_evolution_factor_sensor_spawner_display", 42},
-					  caption = {"rescaled_evolution_factor_sensor_spawner_caption"}}
-   
-		remote.call("EvoGUI", "create_remote_sensor", sensor_data)
-   
-		sensor_data = {mod_name = "Evolve Factor",
-					   name = "rescaled_evolution_factor_sensor_anteile",
-					   text = {"rescaled_evolution_factor_sensor_anteile_display", 42},
-					   caption = {"rescaled_evolution_factor_sensor_anteile_caption"}}
-   
-		remote.call("EvoGUI", "create_remote_sensor", sensor_data)
-		
-		global.ist_registriert = true
-	end
-end
-
-function summe_formatieren(v)
-	local s = string.format("%d", math.floor(v))
-	local pos = string.len(s) % 3
-	if pos == 0 then pos = 3 end
-	return string.sub(s, 1, pos) .. string.gsub(string.sub(s, pos+1), "(...)", " %1")
-end
-
-function prozent_formatieren(v)
-    local whole_number = math.floor(v)
-    local fractional_component = math.floor((v - whole_number) * 10)
-	
-	return string.format("%d.%d%%", whole_number, fractional_component)
 end
 
 script.on_event(defines.events.on_tick, pollutiontest)
